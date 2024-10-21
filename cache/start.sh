bab49742c703adbaa672f0996867fd25349ec624
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2024-10-21 18:41:17 +0530

    From POC: Connect app with valkey.

---
+++ cache/start.sh
@@ -0,0 +1,11 @@
+valkey-server /usr/local/etc/valkey/valkey.conf --daemonize yes && sleep 2
+valkey-cli FLUSHALL
+si_val='{"id":1,"key":"test", "url":"'
+si_val=$si_val$HOST
+si_val=$si_val'"}'
+valkey-cli SET si:site1.wb.test "$si_val"
+valkey-cli SET wh:test '{"id":1,"url":"site1.wb.test"}'
+valkey-cli SAVE
+valkey-cli shutdown
+
+valkey-server /usr/local/etc/valkey/valkey.conf
