ffaf52983f3e6ef89f1a5d95c8fd1315bb5d3fe9
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2024-10-21 19:01:17 +0530

    From POC: Add cart endpoint.

--- app/src/index.php
+++ app/src/index.php
@@ -3,7 +3,7 @@
 global $redis;
 
 const WEBHOOK_NS = 'wh:';
-
+const CLIENT_SITE_NS = 'si:';
 
 $redis = new Redis();
 try {
@@ -13,9 +13,40 @@ try {
 	exit;
 }
 
+function init_client(): void {
+	global $site_info, $redis;
+
+	if ( ! isset( $_SERVER['HTTP_X_WB_CLIENT_SITE'] ) ) {
+		header( 'HTTP/1.1 400 Bad Request' );
+		echo 'HTTP_X_WB_CLIENT_SITE header is required';
+		exit;
+	}
+
+	ini_set( 'session.save_handler', 'redis' );
+	ini_set( 'session.save_path', 'tcp://' . getenv('valkey') . ':6379' );
+	$site_info_json = $redis->get( CLIENT_SITE_NS . $_SERVER['HTTP_X_WB_CLIENT_SITE'] );
+
+	if ( ! $site_info_json ) {
+		header( 'HTTP/1.1 403 Forbidden' );
+		exit;
+	}
+
+	$site_info = json_decode( $site_info_json, true );
+
+	if ( ! isset( $_SESSION ) ) {
+		session_start();
+	}
+
+	header("Access-Control-Allow-Origin: {$site_info['url']}");
+}
+
 $request = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
 
 switch ( $request ) {
+	case '/store/v1/cart':
+		init_client();
+		require __DIR__ . '/store/v1/cart.php';
+		break;
 	case '/webhooks/products':
 		require __DIR__ . '/webhooks/products.php';
 		break;
---
+++ app/src/store/cart.php
@@ -0,0 +1,44 @@
+<?php
+
+global $redis, $site_info;
+
+$session_id = session_id();
+$existing_cart_json = $redis->get( 'cart:' . $session_id );
+$cart = get_cart_object( $existing_cart_json );
+
+if ( ! $existing_cart_json ) {
+	$redis->set( 'cart:' . $session_id, json_encode( $cart ) );
+}
+
+function get_empty_cart_object() : array {
+	return array(
+		'items' => array(),
+		'coupons' => array(),
+		'fees' => array(),
+		'totals' => array(),
+		'shipping_address' => array(),
+		'billing_address' => array(),
+		'needs_shipping' => false,
+		'needs_payment' => true,
+		'payment_requirements' => array(),
+		'has_calculated_shipping' => false,
+		'shipping_rates' => array(),
+		'items_count' => 0,
+		'items_weight' => 0,
+		'cross_sells' => array(),
+		'errors' => array(),
+		'payment_methods' => array(),
+		'extensions' => array(),
+	);
+}
+
+function get_cart_object( $existing_cart_json ) {
+	if ( ! $existing_cart_json ) {
+		return get_empty_cart_object();
+	} else {
+		return json_decode( $existing_cart_json, true );
+	}
+}
+
+header( 'Content-Type: application/json' );
+print_r( json_encode( $cart ) );
