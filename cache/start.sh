256f0bf4bd87292dfcc91d2d51216faf0b02219b
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2024-10-24 15:01:58 +0530

    Remove placeholders.

--- cache/start.sh
+++ cache/start.sh
@@ -1,11 +1,1 @@
-valkey-server /usr/local/etc/valkey/valkey.conf --daemonize yes && sleep 2
-valkey-cli FLUSHALL
-si_val='{"id":1,"key":"test", "url":"'
-si_val=$si_val$HOST
-si_val=$si_val'"}'
-valkey-cli SET si:site1.wb.test "$si_val"
-valkey-cli SET wh:test '{"id":"site_wb_test","url":"site1.wb.test"}'
-valkey-cli SAVE
-valkey-cli shutdown
-
 valkey-server /usr/local/etc/valkey/valkey.conf
