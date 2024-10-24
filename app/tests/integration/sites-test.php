cdea216a0ecd8880dde7eec01c6263c434306dc6
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2024-10-24 16:09:41 +0530

    Add unit tests to check auth.

--- app/tests/integration/sites-test.php
+++ app/tests/integration/sites-test.php
@@ -2,12 +2,16 @@
 
 class SitesTest extends PHPUnit\Framework\TestCase {
 
-	public function test_site_can_be_registered() {
+	private function get_new_site_info() {
 		$site_id = get_new_site_id();
-		$site_info = array(
+		return array(
 			'id' => $site_id,
 			'url' => $site_id . '.integration.test',
 		);
+	}
+
+	public function test_site_can_be_registered() {
+		$site_info = $this->get_new_site_info();
 
 		$client = new GuzzleHttp\Client();
 		$response = $client->post(
@@ -18,5 +22,27 @@ class SitesTest extends PHPUnit\Framework\TestCase {
 
 		$this->assertEquals( 201, $response->getStatusCode() );
 		$this->assertEquals( 'Site created', json_decode( $response->getBody()->getContents(), true )['message'] );
+	}
+
+	public function test_site_endpoint_is_not_available_without_token() {
+		$site_info = $this->get_new_site_info();
+
+		$client = new GuzzleHttp\Client();
+		$response = $client->post(
+			get_test_api_domain() . '/sites',
+			array(
+				'json' => $site_info,
+				'http_errors' => false
+			)
+		);
+
+		$this->assertEquals( 404, $response->getStatusCode() );
+
+		$response = $client->post(
+			get_test_api_domain() . '/sites?token=incorrect',
+			array( 'json' => $site_info, 'http_errors' => false )
+		);
+
+		$this->assertEquals( 403, $response->getStatusCode() );
 	}
 }
